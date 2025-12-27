c7d7c9280b01f5a6bbf6a26982900aa20dd51ea9
alweena07 <alweenaiqbal.07@gmail.com>
2025-12-27 14:04:05 +0530

    update import with absolute path

--- react-sandbox/src/app/routes.tsx
+++ react-sandbox/src/app/routes.tsx
@@ -1,8 +1,8 @@
 import type { RouteObject } from "react-router-dom";
 import { lazy } from "react";
 
-const TodoPage = lazy(() => import("../features/todo").then(mod => ({ default: mod.TodoPage })));
-const HomePage = lazy(() => import("../features/home").then(mod => ({ default: mod.Home })));
+const TodoPage = lazy(() => import("@/features/todo").then(mod => ({ default: mod.TodoPage })));
+const HomePage = lazy(() => import("@/features/home").then(mod => ({ default: mod.Home })));
 
 export const routes: RouteObject[] = [
   { path: "/", element: <HomePage /> },
