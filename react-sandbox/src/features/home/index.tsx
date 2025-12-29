7a56407b58c0726806520c5c15ec8d66e1655f2a
TarunDharmashya <tdharmashya18111998@gmail.com>
2025-12-29 23:04:12 +0530

    Feature: Add Counter

--- react-sandbox/src/features/home/index.tsx
+++ react-sandbox/src/features/home/index.tsx
@@ -1,6 +1,9 @@
 export const Home = () => {
   return <div>
     <div>Welcome to the React Sandbox!</div>
+    <br />
     <a href="/todo">Go to Todo Page</a>
+    <br />
+    <a href="/counter">Go to Counter Page</a>
   </div>;
 }
