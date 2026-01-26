77a2f40a29eb91f2b197c3c8e0bafe5d9408786f
alweena07 <alweenaiqbal.07@gmail.com>
2026-01-26 13:04:06 +0530

    create debounced search component

--- react-sandbox/src/features/todo/TodoPage.tsx
+++ react-sandbox/src/features/todo/TodoPage.tsx
@@ -1,42 +1,40 @@
-import { useState } from "react";
-import { Button } from "@/shared/components/Button"
-import { InputBox } from "@/shared/components/InputBox"
-import type { TodoItem } from "./types";
-import { DisplayList } from "./components/displayList";
-import "./styles.css";
+import { useState } from 'react';
+import { Button } from '@/shared/components/Button';
+import { InputBox } from '@/shared/components/InputField/InputBox';
+import type { TodoItem } from './types';
+import { DisplayList } from './components/displayList';
+import './styles.css';
 
 export const TodoPage = () => {
-  const [inputValue, setInputValue] = useState("");
-  const [todos, setTodos] = useState<TodoItem[]>([]);
+    const [inputValue, setInputValue] = useState('');
+    const [todos, setTodos] = useState<TodoItem[]>([]);
 
-  const addTodo = () => {
-    const newTodo: TodoItem = {
-      id: todos.length + 1,
-      text: inputValue,
-      completed: false,
+    const addTodo = () => {
+        const newTodo: TodoItem = {
+            id: todos.length + 1,
+            text: inputValue,
+            completed: false,
+        };
+        setInputValue('');
+        setTodos([...todos, newTodo]);
     };
-    setInputValue("");
-    setTodos([...todos, newTodo]);
-  }
 
-  const changeStatus = (id: number, checked: boolean) => {
-    setTodos(todos.map(todo => 
-      todo.id === id ? { ...todo, completed: checked } : todo
-    ));
-  }
+    const changeStatus = (id: number, checked: boolean) => {
+        setTodos(todos.map((todo) => (todo.id === id ? { ...todo, completed: checked } : todo)));
+    };
 
-  const deleteTodo = (id: number) => {
-    setTodos(todos.filter(todo => todo.id !== id));
-  }
+    const deleteTodo = (id: number) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
 
-  return (
-    <div>
-      <h1>Todo List</h1>
-      <div className="add-todo-container">
-        <InputBox placeholder="Enter a new todo" onChange={setInputValue} value={inputValue} />
-        <Button onClick={() => addTodo()} label="Add Todo" />
-      </div>
-      <DisplayList todos={todos} onChange={changeStatus} onDelete={deleteTodo} />
-    </div>
-  )
-}
+    return (
+        <div>
+            <h1>Todo List</h1>
+            <div className="add-todo-container">
+                <InputBox placeholder="Enter a new todo" onChange={setInputValue} value={inputValue} />
+                <Button onClick={() => addTodo()} label="Add Todo" />
+            </div>
+            <DisplayList todos={todos} onChange={changeStatus} onDelete={deleteTodo} />
+        </div>
+    );
+};
